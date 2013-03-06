151b6ec4d268eebd5e2da8a900ac98ff3def30dd
Agustin Huerta <ahuearr@gmail.com>
2013-03-06 23:43:51 +0100

    MenuItem

--- src/com/gusycorp/nim/activity/PartidasActivity.java
+++ src/com/gusycorp/nim/activity/PartidasActivity.java
@@ -14,6 +14,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -38,7 +39,8 @@ public class PartidasActivity extends ListActivity implements OnClickListener {
 
 	@Override
 	public boolean onCreateOptionsMenu(final Menu menu) {
-		getMenuInflater().inflate(R.menu.partidas, menu);
+		MenuItem nuevaPartida = menu.add(0, 1, 0, getString(R.string.bt_nueva_partida));
+		MenuItem configuracionPartida = menu.add(0, 2, 1, getString(R.string.bt_grabar_configuracion));
 		return true;
 	}
 
